baacfce78a62297006b6e0482733a08f3910424d
KATARI REVATHI <katari.revathi@gmail.com>
2024-11-27 16:23:31 +0530

    files

--- build_files.sh
+++ build_files.sh
@@ -9,27 +9,12 @@
 # Install dependencies from requirements.txt
 /usr/bin/python3.9 -m pip install -r requirements.txt
 
-# Set up the virtual environment
-# Create a virtual environment if it doesn't exist
-if [ ! -d "env" ]; then
-  /usr/bin/python3.9 -m venv env
-fi
+# Create static directory if it doesn't exist
+mkdir -p /vercel/path0/static
 
-# Activate the virtual environment
-source env/bin/activate
-
-# Install dependencies in the virtual environment
-pip install -r requirements.txt
-
-# Ensure that the static directory exists in the Vercel public folder
-mkdir -p /vercel/path0/public/static
-
-# Collect static files to the correct directory
-python manage.py collectstatic --noinput
+# Collect static files
+python3.9 manage.py collectstatic --noinput
 
 # Apply database migrations
-python manage.py makemigrations
-python manage.py migrate
-
-# Deactivate the virtual environment
-deactivate
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
