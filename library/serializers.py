e28f3dd7f7440b6227398906de1cf3469d0bc3b7
KATARI REVATHI <katari.revathi@gmail.com>
2024-12-11 20:42:37 +0530

    individual layers display

--- library/serializers.py
+++ library/serializers.py
@@ -1,35 +1,69 @@
 from rest_framework import serializers
 from .models import LayerA, LayerB, LayerC, LayerD, LayerE
 
+# class LayerESerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = LayerE
+#         fields = ['id', 'layer_e_name']
+
+# class LayerDSerializer(serializers.ModelSerializer):
+#     layer_es = LayerESerializer(many=True, read_only=True)
+
+#     class Meta:
+#         model = LayerD
+#         fields = ['id', 'layer_d_name', 'slug', 'layer_es']
+
+# class LayerCSerializer(serializers.ModelSerializer):
+#     layer_ds = LayerDSerializer(many=True, read_only=True)
+
+#     class Meta:
+#         model = LayerC
+#         fields = ['id', 'layer_c_name', 'slug', 'layer_ds']
+
+# class LayerBSerializer(serializers.ModelSerializer):
+#     layer_cs = LayerCSerializer(many=True, read_only=True)
+
+#     class Meta:
+#         model = LayerB
+#         fields = ['id', 'layer_b_name', 'slug', 'layer_cs']
+
+# class LayerASerializer(serializers.ModelSerializer):
+#     layer_bs = LayerBSerializer(many=True, read_only=True)
+
+#     class Meta:
+#         model = LayerA
+#         fields = ['id', 'layer_a_name', 'slug', 'layer_bs']
+
+
 class LayerESerializer(serializers.ModelSerializer):
     class Meta:
         model = LayerE
-        fields = ['id', 'layer_e_name']
+        fields = ['id', 'layer_e_name']  # Include only the necessary fields
 
 class LayerDSerializer(serializers.ModelSerializer):
-    layer_es = LayerESerializer(many=True, read_only=True)
 
     class Meta:
         model = LayerD
-        fields = ['id', 'layer_d_name', 'slug', 'layer_es']
+        fields = ['id', 'layer_d_name', 'slug']  # Include only the necessary fields
 
 class LayerCSerializer(serializers.ModelSerializer):
-    layer_ds = LayerDSerializer(many=True, read_only=True)
+    
 
     class Meta:
         model = LayerC
-        fields = ['id', 'layer_c_name', 'slug', 'layer_ds']
+        fields = ['id', 'layer_c_name', 'slug']  # Include only the necessary fields
 
 class LayerBSerializer(serializers.ModelSerializer):
-    layer_cs = LayerCSerializer(many=True, read_only=True)
+   
 
     class Meta:
         model = LayerB
-        fields = ['id', 'layer_b_name', 'slug', 'layer_cs']
+        fields = ['id', 'layer_b_name', 'slug']  # Include only the necessary fields
 
 class LayerASerializer(serializers.ModelSerializer):
-    layer_bs = LayerBSerializer(many=True, read_only=True)
+    
 
     class Meta:
         model = LayerA
-        fields = ['id', 'layer_a_name', 'slug', 'layer_bs']
+        fields = ['id', 'layer_a_name', 'slug']  # Include only the necessary fields
+
