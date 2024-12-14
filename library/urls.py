6cb78d8af38e9f6b772040e1e708204e36a8cc8e
KATARI REVATHI <katari.revathi@gmail.com>
2024-12-14 11:02:44 +0530

    layerfurl

--- library/urls.py
+++ library/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LayerAListView
+from .views import LayerAListView, LayerFListCreateView
 
 urlpatterns = [
     path('layera/', LayerAListView.as_view(), name='layera-list'),
@@ -7,6 +7,6 @@ urlpatterns = [
     # path('layerc/', LayerCListView.as_view(), name='layerc-list'),
     # path('layerd/', LayerDListView.as_view(), name='layerd-list'),
     # path('layere/', LayerEListView.as_view(), name='layere-list'),
-    # path('layerf/', LayerFListCreateView.as_view(), name='layerf-list-create'),
+     path('layerf/', LayerFListCreateView.as_view(), name='layerf-list-create'),
     # path('layerf/<int:pk>/', LayerFDetailView.as_view(), name='layerf-detail'),
 ]
